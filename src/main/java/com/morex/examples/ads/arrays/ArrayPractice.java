c5059982005da8b6199d8b3584d360653b11e35a
morteza.pouladi@allianz.com <morteza.pouladi@allianz.com>
2023-03-20 16:38:25 +0700

    add a better output msg

--- src/main/java/com/morex/examples/ads/arrays/ArrayPractice.java
+++ src/main/java/com/morex/examples/ads/arrays/ArrayPractice.java
@@ -1,11 +1,10 @@
 package com.morex.examples.ads.arrays;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class ArrayPractice {
 	public static void main(String[] args) {
-		reverseArray(new Integer[]{5,1,8,2,4});
+		reverseArray(new Integer[]{5, 1, 8, 2, 4});
 	}
 
 	private static void reverseArray(Integer[] array) {
@@ -13,6 +12,7 @@ public class ArrayPractice {
 			System.out.println("array is empty!");
 			return;
 		}
+		System.out.println("the array: " + Arrays.toString(array));
 		int start = 0, end = array.length - 1;
 		while (start < end) {
 			int temp = array[start];
@@ -21,6 +21,6 @@ public class ArrayPractice {
 			start++;
 			end--;
 		}
-		System.out.println(Arrays.toString(array)); // output: [4, 2, 8, 1, 5]
+		System.out.println("reverseArray() result : " + Arrays.toString(array)); // output: [4, 2, 8, 1, 5]
 	}
 }
